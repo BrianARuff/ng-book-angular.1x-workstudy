998f4ff9941d1a7c017ecea99c0a1c47ef58fc87
brianaruff <briananthonyruff@gmail.com>
2020-01-10 00:14:04 -0500

    comment added on data binding best practices

--- helloworld/controller.js
+++ helloworld/controller.js
@@ -1,7 +1,7 @@
 angular
   .module("myApp", [])
   .controller("MyController", function($scope, $timeout) {
-    $scope.clock = { now: "" };
+    $scope.clock = { now: "" }; // since JS passes by value instead of reference, it's best practice to bind data as a property on an object rather than the raw object itself. This means instead of passing the value 3 and all of its properties associates with  it, we would make an object like "const num = {three: 3}", and then call "num.three" in the view layer. This makes everything much faster because it only has to find the reference to the object and display it and not load all the object properties associated with it and still display it.
     const updateClock = function() {
       $scope.clock.now = new Date().getSeconds();
       $timeout(() => {
